f44887ce036603c17a0b46799ccef17fef8f9a79
alinosratipour <alinosratipour@gmail.com>
2024-01-29 14:52:04 +0000

    deploy node 2

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const loginUser = require("./routes/userRoute/userLoginRoute.js");
 const dashboard = require("./routes/dashboard/dashboardRoute.js");
 const verifyAuth = require("./routes/verifyjwt");
 //middelware
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: 'http://ali-s3-todo-app.s3-website.eu-west-2.amazonaws.com' }));
 app.use(express.json()); // allows access to req.body 
 
 //app.use(express.static("./client/build"))
+
 
 app.use("/", todos);
 app.use("/", updateTodos);
