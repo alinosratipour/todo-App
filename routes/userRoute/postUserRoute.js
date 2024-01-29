1a1382f7cac4f4a8c14094b89945369e9e8f0baa
alinosratipour <alinosratipour@gmail.com>
2024-01-29 18:54:14 +0000

    yml last working status

--- routes/userRoute/postUserRoute.js
+++ routes/userRoute/postUserRoute.js
@@ -7,31 +7,28 @@ const jwtGenerator = require("../../utils/jwtGenerator");
 router.post("/signup", async (req, res) => {
   try {
     const { firstname, lastname, email, password } = req.body;
-    
+
     //Encrypt password
     const salt = await bcrypt.genSalt(10);
     bcryptpassword = await bcrypt.hash(password, salt);
 
-    // Validate email does not exist 
+    // Validate email does not exist
     const query = "SELECT * FROM users WHERE email = $1";
-    const checkuser = await pool.query(
-      query,[email]
+    const checkuser = await pool.query(query, [email]);
+
+    if (checkuser.rowCount) {
+      res.status(401).json({ errors: [{ msg: "User already Exist" }] });
+    }
+
+    const newuser = await pool.query(
+      "INSERT INTO users (firstname, lastname, email,password) VALUES($1,$2,$3,$4) RETURNING * ",
+      [firstname, lastname, email, bcryptpassword]
     );
 
-if (checkuser.rowCount) {
-   res.status(401).json({ errors: [{ msg: "User already Exist" }] });
-}
-
-  const newuser = await pool.query(
-    "INSERT INTO users (firstname, lastname, email,password) VALUES($1,$2,$3,$4) RETURNING * ",
-    [firstname, lastname, email, bcryptpassword]
-  );
-
- //generating our jwt token 
- const token = jwtGenerator(newuser.rows[0].id);
-   res.json({token})
-  //res.json({ "success": "New graduate is created", "graduate": newuser.rows[0] })
-  
+    //generating our jwt token
+    const token = jwtGenerator(newuser.rows[0].id);
+    res.json({ token });
+    //res.json({ "success": "New graduate is created", "graduate": newuser.rows[0] })
   } catch (err) {
     console.error(err.message);
   }
