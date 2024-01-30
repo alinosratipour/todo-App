3a9543634c04a44282ee1549bb0a76e3aa3a83aa
alinosratipour <alinosratipour@gmail.com>
2024-01-30 12:44:35 +0000

    refactor code

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const dashboard = require("./routes/dashboard/dashboardRoute.js");
 const verifyAuth = require("./routes/verifyjwt");
 //middelware
 
-
-app.options('*', cors());
-app.use(cors({
-    origin: 'http://ali-s3-todo-app.s3-website.eu-west-2.amazonaws.com',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type',
-  }));
+app.use(
+  cors({
+    origin: "http://ali-s3-todo-app.s3-website.eu-west-2.amazonaws.com",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type",
+  })
+);
 app.use(cors());
-app.use(express.json()); // allows access to req.body 
+app.use(express.json()); // allows access to req.body
 
 //app.use(express.static("./client/build"))
 
@@ -32,45 +32,44 @@ app.use("/", loginUser);
 ///app.use("/", verifyAuth);
 app.use("/", dashboard);
 
-if(process.env.NODE_ENV === "production"){
-    // serve static content
-    // npm run build
-    app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === "production") {
+  // serve static content
+  // npm run build
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 // get a todo
-app.get("/todos/:id", async(req, res) => {
-const {id} = req.params;
-try{
-const findtodo = await pool.query("SELECT * FROM mytodo WHERE todo_id = $1",[id]);
+app.get("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findtodo = await pool.query(
+      "SELECT * FROM mytodo WHERE todo_id = $1",
+      [id]
+    );
     res.json(findtodo.rows[0]);
-}catch(err){
+  } catch (err) {
     console.error(err.message);
-}
-
+  }
 });
-
 
 // delete a todo
-app.delete("/todos/:id" , async (req, res)=>{
-
-    try{ 
-      const { id } = req.params;
-      const deleteTodo = pool.query("DELETE FROM mytodo WHERE todo_id = $1",
-       [id]
-       );
-      //res.json("Todo Deleted");
-      res.json({ success: "Graduate is deleted" });
-    } catch(err){
-      console.error(err.message);
-    }
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = pool.query("DELETE FROM mytodo WHERE todo_id = $1", [
+      id,
+    ]);
+    //res.json("Todo Deleted");
+    res.json({ success: "Graduate is deleted" });
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
-
-app.get("*", (req , res)=>{
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>{
-    console.log(`server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server running at port ${PORT}`);
+});
