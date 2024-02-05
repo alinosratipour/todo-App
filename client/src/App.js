42f0760ec49dfd43807ecd94a9a727dee6923465
alinosratipour <alinosratipour@gmail.com>
2024-02-05 13:56:55 +0000

    test 7

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Signup from "./components/Signup";
 import SecureRoute from "./components/SecureRoute";
 import NavBar from "./components/NavBar";
 import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -17,9 +18,10 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/signup" render={() => <Signup />} />
-              <SecureRoute exact path="/login" />
+              {/* <SecureRoute exact path="/login" /> */}
               {/* <SecureRoute exact path="/dashboard" /> */}
               <Route exact path="/dashboard" render={() => <Dashboard />} />
+              <Route exact path="/login" render={() => <Login />} />
             </Switch>
           </div>
         </Router>
