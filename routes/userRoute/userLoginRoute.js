2ca041a4ad439bc0abae924136a2fe31b3260e08
alinosratipour <alinosratipour@gmail.com>
2024-02-06 11:58:20 +0000

    refactor 16

--- routes/userRoute/userLoginRoute.js
+++ routes/userRoute/userLoginRoute.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const jwtGenerator = require("../../utils/jwtGenerator");
 const authorization = require("../../middleware/authorization");
 // Login Route
-router.post("/login", authorization, async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     // Destracture the req.body
     const { email, password } = req.body;
@@ -29,10 +29,10 @@ router.post("/login", authorization, async (req, res) => {
       return res.status(401).json("Password or email is Wrong");
     }
 
-     const token = jwtGenerator(userPass.rows[0].id);
+     //const token = jwtGenerator(userPass.rows[0].id);
      console.log("Generated Token:", token);
-     res.json({token});
-    //res.json({ token: "login sucess" });
+    // res.json({token});
+    res.json({ token: "login sucess" });
   } catch (err) {
     console.error(err.message);
     res.status(500).send("server error");
